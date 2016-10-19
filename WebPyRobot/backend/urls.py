22aadd0bab1a50f72702d3d8df1fb76b16186677
Mickaël Bernardini <contact@mickael-bernardini.fr>
2016-10-19 18:04:09 +0200

    Create routes for the webpyrobot projet

---
+++ WebPyRobot/backend/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'backend'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^fight/$', views.fight, name='fight'),
+    url(r'^fight/(?P<pk>[0-9]+)/$', views.figthdetail, name='fightdetail'),
+    url(r'^editor/$', views.editor, name='editor'),
+    url(r'^editor/(?P<pk>[0-9]+)/$', views.editordetail, name='editordetail'),
+    url(r'^market/$', views.market, name='market'),
+    url(r'^help/$', views.help, name='help'),
+]
--- WebPyRobot/backend/views.py
+++ WebPyRobot/backend/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('page index')
+
+
+def login(request):
+    return HttpResponse('page login')
+
+
+def logout(request):
+    return HttpResponse('page logout')
+
+
+def signup(request):
+    return HttpResponse('page signup')
+
+
+def fight(request):
+    return HttpResponse('page de fight')
+
+
+def figthdetail(request, pk):
+    return HttpResponse('page figthDetails pour ' + pk)
+
+
+def editor(request):
+    return HttpResponse('page de l editor')
+
+
+def editordetail(request, pk):
+    return HttpResponse('page de l editor pour ' + pk)
+
+
+def market(request):
+    return HttpResponse('page du market')
+
+
+def help(request):
+    return HttpResponse('page du je suis perdu')
