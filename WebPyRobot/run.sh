2efb96ae6db52ff49af340e54e43ef0d03f7d5d7
Azael <yohan-roux@hotmail.fr>
2016-10-19 16:00:13 +0200

    Update PYTHON3 var

--- WebPyRobot/run.sh
+++ WebPyRobot/run.sh
@@ -5,7 +5,7 @@ if [ "$PYTHON_VERSION" != "3" ]; then
 	PYTHON_CMD="python3"
 	PYTHON3_VERSION=$($PYTHON_CMD --version | cut -d\  -f 2 | cut -d. -f 1)
 
-	if [ "$PYTHON_VERSION" != "3" ]; then
+	if [ "$PYTHON3_VERSION" != "3" ]; then
 		echo "PYTHON 3 not found"
 		echo "Please install pyton3 & pip"
 		exit 1
